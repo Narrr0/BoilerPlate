77e32b9007543b88834208a7cadaba10a6d10399
unknown <051ssaa@naver.com>
2021-01-18 22:54:32 +0900

    protect info

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Narrr0:nar106g5446!@boilerplate.wdya5.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -13,7 +20,17 @@ mongoose.connect('mongodb+srv://Narrr0:nar106g5446!@boilerplate.wdya5.mongodb.ne
 .catch((error)=>console.log(error));
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요!')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res)=>{
+  const user = new User(req.body);
+  user.save((err, userInfo) =>{
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
